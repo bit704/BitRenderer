64e0cb69976fa0d6948ee29b3ad09a40ad1177b9
bit704 <mhn.bit@qq.com>
2023-12-24 21:15:42 +0800

    refactor: style

--- BitRenderer/hittable_list.h
+++ BitRenderer/hittable_list.h
@@ -4,10 +4,10 @@
 #ifndef HITTABLE_LIST_H
 #define HITTABLE_LIST_H
 
-#include "hittable.h"
-
 #include <memory>
 #include <vector>
+
+#include "hittable.h"
 
 using std::shared_ptr;
 using std::make_shared;
